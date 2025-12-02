b2ac42dd30c5d128f2edce9dcf40d33fcbd0214d
cls <awetimothy77@gmail.com>
2025-12-02 15:36:32 +0100

    Update

--- src/Chapter6/RandomNumber.java
+++ src/Chapter6/RandomNumber.java
@@ -9,9 +9,27 @@ public class RandomNumber {
  c) 10, 20, 30, 40.
          */
 SecureRandom randomNumbers = new SecureRandom();
-for (int counter=1;counter<=6;counter++) {
-    int a =1+ 3 * randomNumbers.nextInt(6);
-    System.out.print(a+" ");
+//for (int counter=1;counter<=6;counter++) {
+    //int a =3 * randomNumbers.nextInt(5);
+    int scale =6;
+    int random=1+randomNumbers.nextInt(1);
+    int nextNumber=random;
+
+    for (int number=1;number<=scale;number++){
+
+        System.out.print(nextNumber+" ");
+        nextNumber*=2;
+    }
+        /*int scale =4;
+        int random=0+randomNumbers.nextInt(1);
+        int nextNumber=random;
+
+        for (int number=1;number<=scale;number++){
+            nextNumber+=10;
+            System.out.print(nextNumber+" ");
+        }*/
+
+
 }
     }
-}
+//}
